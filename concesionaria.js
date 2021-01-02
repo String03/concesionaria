9bc879a74781e60452898d14ee07bdec46e46d4e
analauramoyano <40239185+analauramoyano@users.noreply.github.com>
2021-01-02 15:06:30 -0300

    Diagramas

--- concesionaria.js
+++ concesionaria.js
@@ -1,26 +1,44 @@
-var vehiculos = [
-    {
-        Marca: "Peugeot", Modelo: "206", Puertas: 4, Precio: 200000, Auto: true
-    },
-    {
-        Marca: "Honda", Modelo: "Titan", Clindrada: "125cc", Precio: 60000, Auto: false
-    },
-    {
-        Marca: "Peugeot", Modelo: "208", Puertas: 5, Precio: 250000, Auto: true
-    },
-    {
-        Marca: "Yamaha", Modelo: "YBR", Clindrada: "160cc", Precio: 80500.50, Auto: false
-    }
-
-];
-
-function ImprimirVehiculos(vehiculos){
-    for(var i in vehiculos){
-        console.log(DetalleVehiculo(vehiculos[i]));
+class Vehiculo
+{
+    constructor(vehiculo){
+        this.Marca = vehiculo.Marca;
+        this.Modelo = vehiculo.Modelo;
+        this.Puertas = vehiculo.Puertas;
+        this.Clindrada = vehiculo.Clindrada;
+        this.Precio = vehiculo.Precio;
     }
 }
 
-function DetallePuerta(vehiculo){
+let vehiculos = [
+    
+    new Vehiculo( {
+        Marca: "Peugeot", Modelo: "206", Puertas: 4, Precio: 200000, Auto: true
+    }),
+   
+    new Vehiculo( {
+        Marca: "Honda", Modelo: "Titan", Clindrada: "125cc", Precio: 60000, Auto: false
+    }),
+   
+    new Vehiculo(  {
+        Marca: "Peugeot", Modelo: "208", Puertas: 5, Precio: 250000, Auto: true
+    }),
+  
+    new Vehiculo( {
+        Marca: "Yamaha", Modelo: "YBR", Clindrada: "160cc", Precio: 80500.50, Auto: false
+    })
+   
+
+];
+
+class VehiculoService
+{
+    ImprimirVehiculos(vehiculos){
+    for(var i in vehiculos){
+        console.log(this.DetalleVehiculo(vehiculos[i]));
+    }
+}
+
+DetallePuerta(vehiculo){
     if(vehiculo.Auto){
         return " // Puertas: " + vehiculo.Puertas
     }
@@ -29,61 +47,64 @@ function DetallePuerta(vehiculo){
     }
 }
 
-function DetalleVehiculo(vehiculo){
-    return "Marca: " + vehiculo.Marca + " // Modelo: " + vehiculo.Modelo  + DetallePuerta(vehiculo)+ " // Precio: " + DetallePrecio(vehiculo.Precio);  
+DetalleVehiculo(vehiculo){
+    return "Marca: " + vehiculo.Marca + " // Modelo: " + vehiculo.Modelo  + this.DetallePuerta(vehiculo)+ " // Precio: " + this.DetallePrecio(vehiculo.Precio);  
 }
 
-function DetallePrecio(precio){
+DetallePrecio(precio){
     return new Intl.NumberFormat('es-AR', {currency: 'ARS', style: 'currency'}).format(precio);
 }
 
-function CompararPrecioAscendente(precioA, precioB){
+CompararPrecioAscendente(precioA, precioB){
     return precioA.Precio - precioB.Precio;
 }
 
-function CompararPrecioDescendiente(precioA,precioB){
+CompararPrecioDescendiente(precioA,precioB){
     return precioB.Precio - precioA.Precio;
 }
 
-function ImprimirVehiculoMasCaro(vehiculos){
-    var vehiculoCaro = vehiculos.sort(CompararPrecioDescendiente)[0];
+ImprimirVehiculoMasCaro(vehiculos){
+    var vehiculoCaro = vehiculos.sort(this.CompararPrecioDescendiente)[0];
     console.log("Vehículo más caro: "+ vehiculoCaro.Marca + " " + vehiculoCaro.Modelo);
 }
 
-function ImprimirVehiculoMasBarato(vehiculos){
-    var vehiculoBarato = vehiculos.sort(CompararPrecioAscendente)[0];
+ImprimirVehiculoMasBarato(vehiculos){
+    var vehiculoBarato = vehiculos.sort(this.CompararPrecioAscendente)[0];
     console.log("Vehículo más barato: "+ vehiculoBarato.Marca + " " + vehiculoBarato.Modelo);
 }
 
 
-function CompararVehiculoY(vehiculo){
+CompararVehiculoY(vehiculo){
     return vehiculo.Modelo.includes('Y');
 }
 
-function ImprimirVehiculoY(vehiculos){
-    var vehiculoConY = vehiculos.filter(CompararVehiculoY)[0]; 
-    console.log("Vehículo que contiene en el modelo la letra ‘Y’: " + vehiculoConY.Marca + " " + vehiculoConY.Modelo + " " + DetallePrecio(vehiculoConY.Precio));
+ImprimirVehiculoY(vehiculos){
+    var vehiculoConY = vehiculos.filter(this.CompararVehiculoY)[0]; 
+    console.log("Vehículo que contiene en el modelo la letra ‘Y’: " + vehiculoConY.Marca + " " + vehiculoConY.Modelo + " " + this.DetallePrecio(vehiculoConY.Precio));
 }
 
 
-function ImprimirVehiculosOrdenados(vehiculos){
-    var vehiculosOrdenados = vehiculos.sort(CompararPrecioDescendiente);
+ImprimirVehiculosOrdenados(vehiculos){
+    var vehiculosOrdenados = vehiculos.sort(this.CompararPrecioDescendiente);
     console.log("Vehículos ordenados por precio de mayor a menor:");
     for(var i in vehiculosOrdenados){
         console.log(vehiculosOrdenados[i].Marca + " " + vehiculosOrdenados[i].Modelo);
     }
 }
 
-function ImprimirIgual(){
+ImprimirIgual(){
     return console.log("=============================");
 }
 
+}
 
-ImprimirVehiculos(vehiculos);
-ImprimirIgual();
-ImprimirVehiculoMasCaro(vehiculos);
-ImprimirVehiculoMasBarato(vehiculos);
-ImprimirVehiculoY(vehiculos);
-ImprimirIgual();
-ImprimirVehiculosOrdenados(vehiculos);
+let vehiculoService = new VehiculoService()
+
+vehiculoService.ImprimirVehiculos(vehiculos);
+vehiculoService.ImprimirIgual();
+vehiculoService.ImprimirVehiculoMasCaro(vehiculos);
+vehiculoService.ImprimirVehiculoMasBarato(vehiculos);
+vehiculoService.ImprimirVehiculoY(vehiculos);
+vehiculoService.ImprimirIgual();
+vehiculoService.ImprimirVehiculosOrdenados(vehiculos);
 
